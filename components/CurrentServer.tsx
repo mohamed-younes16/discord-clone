a88e9911b64e9ecfeff77d543c2464c733a0d006
mohamed-younes16 <bettiouim77@gmail.com>
2023-11-10 05:57:47 +0100

    fixed

--- components/CurrentServer.tsx
+++ components/CurrentServer.tsx
@@ -8,7 +8,7 @@ const CurrentServer  = ({id}:{id:string}) => {
 
 
   return  <div className={`absolute   -left-0 w-1 bg-emerald-600  rounded-xl
-   ${params.serverid == id ? "w-1 h-full " :" w-0 h-0" } transition-all`}></div>
+   ${params?.serverid == id ? "w-1 h-full " :" w-0 h-0" } transition-all`}></div>
 
     
   
