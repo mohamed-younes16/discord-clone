4ba29586b70a11d4ceca436ebccb234382abe9da
mohamed-younes16 <bettiouim77@gmail.com>
2023-11-13 13:04:24 +0100

    fixed

--- components/LiveVidAud.tsx
+++ components/LiveVidAud.tsx
@@ -49,7 +49,7 @@ const  { theme,} = useTheme()
 
   return (
     <LiveKitRoom
-      video={video}
+      video={true}
       audio={audio}
       token={token}
       connectOptions={{ autoSubscribe: false }}
@@ -59,33 +59,32 @@ const  { theme,} = useTheme()
       style={{ height: '100dvh' }}
       
     >
-      {/* Your custom component with basic video conferencing functionality. */}
-      <MyVideoConference user = {user}  />
+      
+      <VideoConference/>
   
       <RoomAudioRenderer />
-      {/* Controls for the user to start/stop audio, video, and screen 
-      share tracks and to leave the room. */}
-      <ControlBar />
+    
+   
     </LiveKitRoom>
   );
 }
 
-function MyVideoConference({user}:{user:UserDocument}) {
-  // `useTracks` returns all camera and screen share tracks. If a user
-  // joins without a published camera track, a placeholder track is returned.
-  const tracks = useTracks(
-    [
-      { source: Track.Source.Camera, withPlaceholder: true },
-      { source: Track.Source.ScreenShare, withPlaceholder: false },
-    ],
-    { onlySubscribed: false },
-  );
-  return (
-    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
-      {/* The GridLayout accepts zero or one child. The child is used
-      as a template to render all passed in tracks. */}
-        
-      <ParticipantTile/>
-    </GridLayout>
-  );
-}
+// function MyVideoConference() {
+//   // `useTracks` returns all camera and screen share tracks. If a user
+//   // joins without a published camera track, a placeholder track is returned.
+//   const tracks = useTracks(
+//     [
+//       { source: Track.Source.Camera, withPlaceholder: true },
+//       { source: Track.Source.ScreenShare, withPlaceholder: false },
+//     ],
+//     { onlySubscribed: false },
+//   );
+//   return (
+//     <GridLayout tracks={tracks} 
+//     style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+  
+//         <VideoConference/>
+//       <ParticipantTile/>
+//     </GridLayout>
+//   );
+// }
